7f58eaeb853a0cb723efd29778302cb45836dbb3
PuskayToDay <113018112+PuskayToDay@users.noreply.github.com>
2023-03-17 23:22:45 +0300

    Add files via upload

--- script-0.js
+++ script-0.js
@@ -12939,7 +12939,7 @@ Url: ${Wr(t)}`), !0)
             type: "custom"
         },
         o0 = {
-            Стандартная: {
+            main: {
                 sha: "d494c3bd5d2b5eaeba8934fec33ea08da0e17334",
                 lastUpdated: 1672941040347,
                 version: "5.93.0",
